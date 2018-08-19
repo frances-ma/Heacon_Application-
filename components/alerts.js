0b6b5f5f52eab1eb3c8fdab1198a2d75c6d04d92
frances <frankie.rae1966@gmail.com>
2018-08-19 18:53:32 -0400

    alert page updates

--- components/alerts.js
+++ components/alerts.js
@@ -18,8 +18,26 @@ const alerts = {
 </nav>
 
     <h1 class="alert_text">Health Alerts</h1>
+
      <div class="alert_box">
        <div class="lupus_board">Lupus Board Updates</div>
+       <p class="article_title">Coping with Lupus: How Does Lupus Affect Your State of Mind and Body?</p>
+       <div class="mom">
+          <a href="https://www.kaleidoscopefightinglupus.org/coping-with-lupus-how-does-lupus-affect-your-state-of-mind-and-body/">
+          <img class="coping" src="graphics/coping.jpg"/>
+          </a>
+       </div>
+
+       <div class="dropdown">
+       <button class="dropbtn">Dropdown</button>
+       <div class="dropdown-content">
+         <a href="https://www.lupus.org/research-news">New Studies</a>
+         <a href="https://www.sciencedaily.com/news/health_medicine/lupus/">Headlines</a>
+         <a href="https://www.sciencedaily.com/releases/2018/04/180420122847.htm">New Treatments</a>
+       </div>
+     </div>
+
+
      </div>
 
   `
